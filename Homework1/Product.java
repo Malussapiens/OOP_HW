47011b1cd7ca8d32b59f092fc9a08687d34c2341
Dead_Moroz <malussapiens@gmail.com>
2023-03-06 16:56:13 +0300

    Product class added

---
+++ Homework1/Product.java
@@ -0,0 +1,8 @@
+package Homework1;
+
+public class Product {
+    protected String name = "";
+    protected double price = 0.0;
+    protected double quantity = 0.0;
+    protected String unit = "";
+}
