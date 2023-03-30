3959aa75bf5759fcda7c33bd1eb99a56f5daa4c7
Dead_Moroz <malussapiens@gmail.com>
2023-03-31 02:14:34 +0300

    unused imports removed

--- Homework5/Program/Progarm.java
+++ Homework5/Program/Progarm.java
@@ -1,7 +1,5 @@
 package Homework5.Program;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 public class Progarm {
--- Homework5/Program/Tokenizer.java
+++ Homework5/Program/Tokenizer.java
@@ -1,6 +1,5 @@
 package Homework5.Program;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
