3c8ffca6a086648f302b2f6fdc995db596112921
Dead_Moroz <malussapiens@gmail.com>
2023-04-08 11:39:25 +0300

    Tokenizer.py done

---
+++ Homework7/model/Tokenizer.py
@@ -0,0 +1,28 @@
+from collections import deque as queue
+tokens = queue()
+ops = ['(', ')', '-', '+', '/', '*', '^']
+
+
+def tokenize(expression: str) -> queue:
+    token = ''
+
+    for char in expression:
+        if char in ops:
+            print(char, token)
+            if len(token) > 0:
+                tokens.append(token)
+                token = ''
+            if char == '-':
+                if len(token) == 0:
+                    token += char
+                continue
+            token = char
+            tokens.append(token)
+            token = ''
+        elif str.isdigit(char) or char == '.':
+            token += char
+    tokens.append(token)
+    return tokens
+
+
+
